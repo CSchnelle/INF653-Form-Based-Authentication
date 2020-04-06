44d977dc852cad90de452ad9788483f82d7989b9
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-04-06 01:00:38 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,25 +1,13 @@
 <?php
-    //local development server connection
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    //$password = 'pa55word';
-
-    // Heroku connection
-    /*
-    $dsn = 'mysql:host=AVeryLongURLprovidedforJawsDBhost;dbname=YourJawsDBdbname';
-    $username = 'Your JawsDB username';
-    $password = 'Your JawsDB password';
-    */
-    try {
-        //local development server connection
-        //if using a $password, add it as 3rd parameter
-        $db = new PDO($dsn, $username);
-
-        // Heroku connection
-        //$db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('../errors/database_error.php');
-        exit();
-    }
+    $dsn = 'mysql:host=lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=pugvo3b5oeeq6nux';
+    $username = 't4rk1a7232s3xee0';
+    $password = 'pwg44gpsze39ruug';
+ 
+try {
+  $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+  $error_message = $e->getMessage();
+  include('database_error.php');
+  exit();
+}
 ?>
