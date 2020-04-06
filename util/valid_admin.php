6b17fa5457518d535a372a3655227607f23570a5
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-04-06 03:13:57 -0500

    first commit

---
+++ util/valid_admin.php
@@ -0,0 +1,24 @@
+ <?php
+require_once('model/database.php'); 
+require_once('model/admin_db.php');
+
+$username = ''; 
+$password = '';
+if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) { 
+	$username = $_SERVER['PHP_AUTH_USER']; $password = $_SERVER['PHP_AUTH_PW'];
+}
+if (!is_valid_admin_login($username, $password)) { 
+	header('WWW-Authenticate: Basic realm="Admin"'); 
+	header('HTTP/1.0 401 Unauthorized'); 
+	include('unauthorized.php'); 
+	exit();
+} 
+?>
+
+
+ <?php
+// make sure the user is logged in as a valid administrator 
+	if (!isset($_SESSION['is_valid_admin'])) { 
+		header("Location: ." );
+} 
+?>
