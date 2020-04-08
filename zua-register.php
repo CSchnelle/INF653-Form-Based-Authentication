8b753a32313725106990cb853fdc978ad2179773
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-04-08 13:52:53 -0500

    Update zua-register.php

--- zua-register.php
+++ zua-register.php
@@ -25,17 +25,19 @@
     </header>
 
     <body>
-                
+            <?php if ($username == NULL) { ?>
+                   
             <form method="post" id="register_form" action="">
                 <div class="field-column">
                 <label>Username</label>
                 <div>
                     <input type="text" class="demo-input-box"
-                        name="username"
-                        value="">
+                        name="userName"
+                        value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>">
                 </div>
             </div>
-                <div class="field-column">
+            
+            <div class="field-column">
                 <label>Password</label>
                 <div><input type="password" class="demo-input-box"
                     name="password" value=""></div>
@@ -51,9 +53,6 @@
                 <input type="submit" value="Register" class="button blue">
             </form>
         
-        <p>
-            Click here to sign in if you've already registered. <a href="zua-login.php">Sign in</a>
-        </p>
         <?php } else { 
 
                 $lifetime = 60 * 60 * 24 * 7; //one week
