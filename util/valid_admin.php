418aad034d7658ba44c09bdeebf776608e86a0ed
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-04-08 14:56:26 -0500

    Update valid_admin.php

--- util/valid_admin.php
+++ util/valid_admin.php
@@ -4,18 +4,6 @@ require_once('model/admin_db.php');
 
 $username = ''; 
 $password = '';
-if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) { 
-	$username = $_SERVER['PHP_AUTH_USER']; $password = $_SERVER['PHP_AUTH_PW'];
-}
-if (!is_valid_admin_login($username, $password)) { 
-	header('WWW-Authenticate: Basic realm="Admin"'); 
-	header('HTTP/1.0 401 Unauthorized'); 
-	include('unauthorized.php'); 
-	exit();
-} 
-?>
-
-
  <?php
 
 // if is_valid_admin is not set, return to login page
