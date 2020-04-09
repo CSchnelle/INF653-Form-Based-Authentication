345796f5e16f8bd4720842973430e4f8323f5cd5
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-04-08 21:54:21 -0500

    Update zippy-links.php
    update logout link

--- view/zippy-links.php
+++ view/zippy-links.php
@@ -2,4 +2,5 @@
     <p><a href="?action=show_add_form">Click here</a> to add a vehicle.</p> 
     <p><a href="zua-admin.php?action=list_types">View/Edit Vehicle Types</a></p>
     <p><a href="zua-admin.php?action=list_classes">View/Edit Vehicle Classes</a></p>
+    <p><a href="zua-admin.php?action=logout">Log out</a></p>
 </div>
