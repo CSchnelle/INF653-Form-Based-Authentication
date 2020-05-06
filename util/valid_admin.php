7126d6bcb86a26103c54506db45f9528d046c8f6
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-05-06 16:46:32 -0500

    final commit

--- util/valid_admin.php
+++ util/valid_admin.php
@@ -1,15 +1,5 @@
- <?php
-require_once('model/database.php'); 
-require_once('model/admin_db.php');
-
-$username = ''; 
-$password = '';
- <?php
-
-// if is_valid_admin is not set, return to login page
-	if (isset($_SESSION['is_valid_admin'])) { 
-		header("Location: zua-admin.php");
-	} else { 
-		header("Location: zua-login.php");
-} 
+ <?php 
+    if (!isset($_SESSION['is_valid_admin'])) {
+        header("Location: zua-login.php");
+    }
 ?>
